57fc732eeef04bf6bbf6c4eb44d9abf590bb1244
Atte-Oskari-Rasanen <70641619+Atte-Oskari-Rasanen@users.noreply.github.com>
2020-10-23 12:50:09 +0200

    Update RA3p1.py
    included my attempts of calculating the percentage identity but the scores are too high for some reason.  In the lines 99-100 i was trying to get rid of including the same alignments pair more than once but for some reason it doesnt work so put it into #.

--- RA3p1.py
+++ RA3p1.py
@@ -3,17 +3,17 @@
 create fasta file for mtDNA and for Y chromosome
 
 Do the MSA, maybe save it into another file?
-For MSA we need to choose gap penalty model along with gap penalty values and
+For MSA we need to choose gap penalty model along with gap penalty values and 
 subs matrix
 0. Find the two seqs that give the highest alignment score
 1. look at each two seqs (e.g. a and b), calculate pairwise alignment score
 2.total alignment score is then a sum of all pairwise alig.scores
-- so e.g. if we have seqs a,b,c,d, then the sum S= Sab + Sac + Sad +Sbc + Sbd +
-Scd
+- so e.g. if we have seqs a,b,c,d, then the sum S= Sab + Sac + Sad +Sbc + Sbd + 
+Scd 
 
 
-Then in the final file, do the scoring: transitions, transversions and
-gaps have different penalties.
+Then in the final file, do the scoring: transitions, transversions and 
+gaps have different penalties. 
 
 output MSA results to a file looking like this
 SampleA SampleB IdentityScore Score
@@ -21,9 +21,8 @@ A1 A2 55.2% 35
 A1 A3 51.3% 17
 
 '''
-from itertools import accumulate
 
-#parsing the files into mtDNA fasta and Ychr fasta. Atm Im parsing them into
+#parsing the files into mtDNA fasta and Ychr fasta. Atm Im parsing them into 
 #text files as I cant open fastas on my laptop.
 with open('GeneticData.txt', 'r') as genefile, open('mtDNA.txt','w') as outputdna, open('Ychr.txt','w') as outputY:
     #genefile=genefile.readlines()
@@ -31,21 +30,21 @@ with open('GeneticData.txt', 'r') as genefile, open('mtDNA.txt','w') as outputdn
     seq_listDNA=[]
     Ychr_dict={}
     seq_listY=[]
-
+    
    # name=next(genefile)
-
+    
     seqmtDNA=''
     seqY=''
     for lines in genefile:
         #print(lines)
         if lines.strip():
             #print(lines)    #strip the empty lines (\n)
-
+            
             if lines.startswith("mtDNA"):
                 seqmtDNA=next(genefile).rstrip()
-
+                
             elif lines.startswith("Y"):
-
+                
                 seqY=next(genefile).rstrip()
             elif "hemophilia" in lines:
                 continue
@@ -62,7 +61,7 @@ with open('GeneticData.txt', 'r') as genefile, open('mtDNA.txt','w') as outputdn
 #we have the dictionary with the names as keys and values as seqs, no align. make single dics with one key, one value
 
 with open("mtDNA.txt", 'r') as DNAfile, open("scores.txt","w"):
-
+    
     DNA_dict={}
     lista = []
     idheader = None
@@ -82,55 +81,52 @@ with open("mtDNA.txt", 'r') as DNAfile, open("scores.txt","w"):
     del lista[:]
     #print(DNA_dict)
 
-
+                
 #remember that the program should be able to read the wanted penalties,score values from the file
     transition = ['AG', 'TC', 'GA', 'CT'] #the transition scores
-    NTscore_list=0
+    NTscoreTotal=0
     Seqscore_list=[]  #list for the total scores of NTs when comparing two seqs
     Identical_NTs=0   #for counting the identical ones once we start off the loops
+   
     Header=[]
     SeqAlign={}
+    checked_pair=set()
+    
     for key1, seq1 in DNA_dict.items():
         for key2, seq2 in DNA_dict.items():
             if key1==key2:
                 continue
-            if {key1,key2} not in checked_pair:     #so we only add the key1-key2 comb if its not already in the set, avoiding repeats
-                checked.pair.add(key1,key2)
-            for NTa,NTb in zip(seq1, seq2): #parallel iteration
-
+        #    if {key1,key2} not in checked_pair:     #so we only add the key1-key2 comb if its not already in the set, avoiding repeats
+         #       checked_pair.add({key1,key2})
+                
+            for NTa,NTb in zip(seq1, seq2): #parallel iteration-in one loop calculates the overall score for the alignment
+                
                 if NTa==NTb:
                     if '-' in NTa and '-' in NTb:
                         score=0
-                        #NTscore_list.append(score)
-                        NTscore_list +=score
-
+                        #NTscoreTotal.append(score)
+                        NTscoreTotal +=score
+                        
                     else:
                         score=1
-                        #NTscore_list.append(score)
-                        NTscore_list +=score
+                        NTscoreTotal +=score
                         Identical_NTs+=1   #keep count of identical NTs, identical gaps dont count
+                
                 else: #if the nucleotides are not the same
-                    if '-' in NTa or '-' in NTb: #if one sequence hNTas NTa gNTap when aligned to the other one the score is -1
+                    if '-' in NTa or '-' in NTb: #if one sequence hNTas NTa gNTap when aligned to the other one the score is -1 
                         score = -1
-                        #NTscore_list.append(score)
-                        NTscore_list +=score
+                        NTscoreTotal +=score
+                    
                     else: #if one of them is not a -, it creates a variable nt_sum that is the sum of the two nucleotides and compares them to the transition list
                         NT_both = NTa + NTb
                         if NT_both in transition: # transition:  score is -1
                             score = -1
-                            #NTscore_list.append(score)
-                            NTscore_list +=score
-                        else: #if not transition, then transversion, which has a score of -2 that is added to NTscore_list
+                            NTscoreTotal +=score
+                        else: #if not transition, then transversion, which has a score of -2 that is added to NTscoreTotal
                             score = -2
-                            #NTscore_list.append(score)
-                            NTscore_list +=score
-
-        Seqscore_list.append(NTscore_list)
-        Header=str(key1) +'-'+str(key2)  #a string, works as the header id, i.e. the two person's seqs that are compared
-
-        Sum_for_seq=sum(Seqscore_list)
-        SeqAlign[Header]=Sum_for_seq
-
-        Sum_for_seq=0
-        Seqscore_list.clear()           #trying to reset the list after each iterated over a seq pair
-        print(SeqAlign)
+                        NTscoreTotal +=score
+                    perc_identity=Identical_NTs/len(seq1)*100
+                Header_Seq=str(key1) +' -'+str(key2) + ' : ' + str(NTscoreTotal) + ' ' +str(perc_identity)   #a string, works as the header id, i.e. the two person's seqs that are compared
+                Seqscore_list.append(Header_Seq)
+                NTscoreTotal=0
+    print(Seqscore_list)
